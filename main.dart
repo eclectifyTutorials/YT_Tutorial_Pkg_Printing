d0c6095cf5d5114c098451aea05c0940c893af50
eclectify Tutorials <102626571+eclectifyTutorials@users.noreply.github.com>
2022-05-11 16:15:54 +0200

    Added all files

---
+++ main.dart
@@ -0,0 +1,218 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const customSwatch = MaterialColor(
+    0xFFFF5252,
+    <int, Color>{
+      50: Color(0xFFFFEBEE),
+      100: Color(0xFFFFCDD2),
+      200: Color(0xFFEF9A9A),
+      300: Color(0xFFE57373),
+      400: Color(0xFFEF5350),
+      500: Color(0xFFFF5252),
+      600: Color(0xFFE53935),
+      700: Color(0xFFD32F2F),
+      800: Color(0xFFC62828),
+      900: Color(0xFFB71C1C),
+    },
+  );
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: customSwatch,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Printing"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: _createPdf,
+              child: Text('Create & Print PDF',),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: _displayPdf,
+              child: Text('Display PDF',),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: generatePdf,
+              child: Text('Generate Advanced PDF',),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// create PDF & print it
+  void _createPdf() async {
+    final doc = pw.Document();
+
+    /// for using an image from assets
+    // final image = await imageFromAssetBundle('assets/image.png');
+
+    doc.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Text('Hello eclectify Enthusiast'),
+          ); // Center
+        },
+      ),
+    ); // Page
+
+    /// print the document using the iOS or Android print service:
+    await Printing.layoutPdf(onLayout: (PdfPageFormat format) async => doc.save());
+
+    /// share the document to other applications:
+    // await Printing.sharePdf(bytes: await doc.save(), filename: 'my-document.pdf');
+
+    /// tutorial for using path_provider: https://www.youtube.com/watch?v=fJtFDrjEvE8
+    /// save PDF with Flutter library "path_provider":
+    // final output = await getTemporaryDirectory();
+    // final file = File('${output.path}/example.pdf');
+    // await file.writeAsBytes(await doc.save());
+  }
+
+  /// display a pdf document.
+  void _displayPdf() {
+    final doc = pw.Document();
+    doc.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Text(
+              'Hello eclectify Enthusiast',
+              style: pw.TextStyle(fontSize: 30),
+            ),
+          );
+        },
+      ),
+    );
+
+    /// open Preview Screen
+    Navigator.push(context, MaterialPageRoute(builder:
+        (context) => PreviewScreen(doc: doc),));
+  }
+
+  /// Convert a Pdf to images, one image per page, get only pages 1 and 2 at 72 dpi
+  void _convertPdfToImages(pw.Document doc) async {
+    await for (var page in Printing.raster(await doc.save(), pages: [0, 1], dpi: 72)) {
+      final image = page.toImage(); // ...or page.toPng()
+      print(image);
+    }
+  }
+
+  /// print an existing Pdf file from a Flutter asset
+  void _printExistingPdf() async {
+    // import 'package:flutter/services.dart';
+    final pdf = await rootBundle.load('assets/document.pdf');
+    await Printing.layoutPdf(onLayout: (_) => pdf.buffer.asUint8List());
+  }
+
+  /// more advanced PDF styling
+  Future<Uint8List> _generatePdf(PdfPageFormat format, String title) async {
+    final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
+    final font = await PdfGoogleFonts.nunitoExtraLight();
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: format,
+        build: (context) {
+          return pw.Column(
+            children: [
+              pw.SizedBox(
+                width: double.infinity,
+                child: pw.FittedBox(
+                  child: pw.Text(title, style: pw.TextStyle(font: font)),
+                ),
+              ),
+              pw.SizedBox(height: 20),
+              pw.Flexible(child: pw.FlutterLogo())
+            ],
+          );
+        },
+      ),
+    );
+    return pdf.save();
+  }
+
+  void generatePdf() async {
+    const title = 'eclectify Demo';
+    await Printing.layoutPdf(onLayout: (format) => _generatePdf(format, title));
+  }
+
+}
+
+class PreviewScreen extends StatelessWidget {
+  final pw.Document doc;
+
+  const PreviewScreen({
+    Key? key,
+    required this.doc,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back_outlined),
+        ),
+        centerTitle: true,
+        title: Text("Preview"),
+      ),
+      body: PdfPreview(
+        build: (format) => doc.save(),
+        allowSharing: true,
+        allowPrinting: true,
+        initialPageFormat: PdfPageFormat.a4,
+        pdfFileName: "mydoc.pdf",
+      ),
+    );
+  }
+}
+
